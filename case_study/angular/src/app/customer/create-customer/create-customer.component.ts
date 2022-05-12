772cb28172cb2de26bc69a7866625fd577fdbe18
Huuhuy93 <huynguyen20101993@gmail.com>
2022-05-12 22:53:56 +0700

    ss6

--- case_study/angular/src/app/customer/create-customer/create-customer.component.ts
+++ case_study/angular/src/app/customer/create-customer/create-customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-create-customer',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCustomerComponent implements OnInit {
 
-  constructor() { }
+  createCustomerForm : FormGroup;
+
+  constructor() {
+
+    this.createCustomerForm = new FormGroup({
+
+      customerCode : new FormControl("", [Validators.required, Validators.pattern('^KH-[\\d]{4}')]),
+      customerName : new FormControl("", [Validators.required, Validators.pattern('^[^\\d]+$')]),
+      customerBirthday : new FormControl("", Validators.required),
+      customerGender : new FormControl("", Validators.required),
+      customerIdCard : new FormControl("", [Validators.required, Validators.pattern('^\\d{9}$|\\d{12}$')]),
+      customerPhone : new FormControl("", [Validators.required, Validators.pattern('^(0|\\+84)(\\s|\\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\\d)(\\s|\\.)?(\\d{3})(\\s|\\.)?(\\d{3})$')]),
+      customerEmail : new FormControl("", [Validators.required, Validators.email]),
+      customerAddress : new FormControl("", Validators.required),
+      customerType : new FormControl("", Validators.required),
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  customerTypeList = [
+    {id : 1, name : 'Diamond'},
+    {id : 2, name : 'Platinum'},
+    {id : 3, name : 'Gold'},
+    {id : 4, name : 'Silver'},
+    {id : 5, name : 'Member'},
+  ];
+
+  submitCreateCustomer() {
+    console.log(this.createCustomerForm);
   }
 
 }
--- case_study/angular/src/app/customer/edit-customer/edit-customer.component.ts
+++ case_study/angular/src/app/customer/edit-customer/edit-customer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-customer',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditCustomerComponent implements OnInit {
 
-  constructor() { }
+  editCustomerForm: FormGroup;
+
+  constructor() {
+
+    this.editCustomerForm = new FormGroup({
+      customerId: new FormControl('1'),
+      customerCode: new FormControl("KH-001", [Validators.required,
+        Validators.pattern('^KH-[\\d]{4}')]),
+
+      customerName: new FormControl("Nguyễn Thị Hào", [Validators.required,
+        Validators.pattern('^[^\\d]+$')]),
+
+      customerBirthday: new FormControl("07/11/1970", Validators.required),
+
+      customerGender: new FormControl("Female", Validators.required),
+
+      customerIdCard: new FormControl("643431213", [Validators.required,
+        Validators.pattern('^\\d{9}$|\\d{12}$')]),
+
+      customerPhone: new FormControl("0905423362", [Validators.required,
+        Validators.pattern('^(0|\\+84)(\\s|\\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\\d)(\\s|\\.)?(\\d{3})(\\s|\\.)?(\\d{3})$')]),
+
+      customerEmail: new FormControl("thihao07@gmail.com", [Validators.required, Validators.email]),
+
+      customerAddress: new FormControl("Đà Nẵng", Validators.required),
+
+      customerType: new FormControl("Member", Validators.required),
+    })
+
+  }
 
   ngOnInit(): void {
+  }
+
+  customerTypeList = [
+    {id : 1, name : 'Diamond'},
+    {id : 2, name : 'Platinum'},
+    {id : 3, name : 'Gold'},
+    {id : 4, name : 'Silver'},
+    {id : 5, name : 'Member'},
+  ];
+
+  submitEditCustomer() {
+    console.log(this.editCustomerForm);
   }
 
 }
