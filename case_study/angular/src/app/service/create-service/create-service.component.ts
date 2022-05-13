42cc22404459fca35d4943b936ac5803c71ca725
Huuhuy93 <huynguyen20101993@gmail.com>
2022-05-13 23:02:59 +0700

    ss7

--- case_study/angular/src/app/service/create-service/create-service.component.ts
+++ case_study/angular/src/app/service/create-service/create-service.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-create-service',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateServiceComponent implements OnInit {
 
-  constructor() { }
+  createServiceForm : FormGroup;
+
+  constructor() {
+    this.createServiceForm = new FormGroup({
+      serviceCode : new FormControl("", [Validators.required, Validators.pattern('^DV-[\\d]{4}')]),
+      serviceName : new FormControl("", Validators.required),
+      serviceArea : new FormControl("", [Validators.required, Validators.min(0)]),
+      serviceCost : new FormControl("", [Validators.required, Validators.min(0)]),
+      serviceMaxPeople : new FormControl("", [Validators.required, Validators.min(0)]),
+      roomStandard : new FormControl("", Validators.required),
+      descriptionOtherConvenience : new FormControl("", Validators.required),
+      poolArea : new FormControl("", Validators.min(0)),
+      numberOfFloors : new FormControl("", Validators.min(0)),
+      serviceType : new FormControl("", Validators.required),
+      rentType : new FormControl("", Validators.required),
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  standardList = [
+    {id : 1, name : 'Vip'},
+    {id : 2, name : 'Normal'}
+  ];
+
+  rentTypeList = [
+    {id: 1, name : 'year'},
+    {id: 2, name : 'month'},
+    {id: 3, name : 'day'},
+    {id: 4, name : 'house'}
+  ]
+
+  submitCreateService() {
+    console.log(this.createServiceForm);
   }
 
 }
